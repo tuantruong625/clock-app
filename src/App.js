a16d3d707b01f660ac787a58e674fa2c6408217e
Tuan N. Truong <tuan.truong625@gmail.com>
2021-02-03 15:26:15 -0500

    format time

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import axios from 'axios'
 import TimeDetails from '../src/components/TimeDetails'
 import useToggle from '../src/utils/utils'
+import moment from 'moment'
 
 function App() {
   const [showTimeDetails, setTimeDetails] = useToggle()
   const [quote, setQuote] = React.useState(null)
   const [date, setDate] = React.useState(new Date())
+  const [loading, setLoading] = React.useState('')
 
   const getRandomQuote = React.useCallback(async () => {
-    const response = await axios.get('https://api.quotable.io/random')
-    setQuote(response.data)
+    setLoading('Loading...')
+    try {
+      const response = await axios.get('https://api.quotable.io/random')
+      setQuote(response.data)
+      setLoading('')
+    } catch (e) {
+      console.log(e.message)
+    }
   }, [])
 
   const refreshClock = React.useCallback(() => {
@@ -28,16 +36,21 @@ function App() {
     }
   }, [getRandomQuote, refreshClock])
 
-  const getHours = date.getHours().toString()
-  const getMinutes = date.getMinutes().toString()
+  const time = moment(date).format('HH:mm')
 
   return (
     <div className="container mx-auto">
       <div className="h-screen text-white grid grid-cols-3 gap-4">
         <div className="quote-container p-8 sm:p-24 col-span-full sm:col-span-2 flex items-start">
           <div>
-            <p className="text-gray-100 sm:text-lg">"{quote?.content}"</p>
-            <p className="pt-3 sm:text-lg">{quote?.author}</p>
+            {
+              quote ? (
+                <>
+                  <p className="text-gray-100 sm:text-lg">"{quote?.content}"</p>
+                  <p className="pt-3 sm:text-lg">{quote?.author}</p>
+                </>
+              ) : <p className="text-gray-100 sm:text-lg">{loading}</p>
+            }
           </div>
           <button className="pl-3" onClick={getRandomQuote}>⟳</button>
         </div>
@@ -45,7 +58,7 @@ function App() {
         <div className="col-span-full self-end flex flex-col sm:flex-row sm:justify-between	sm:items-end p-8 sm:p-24">
           <div className="location-info pb-5">
             <p className="uppercase font-light text-gray-100">good morning, it's currently</p>
-            <p className="py-5 font-bold text-8xl uppercase">{getHours}:{getMinutes}</p>
+            <p className="py-5 font-bold text-8xl uppercase">{time}</p>
             <p className="uppercase">in kitchener, ontario</p>
           </div>
 
