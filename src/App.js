bf07d5e8eeaf39339edcc23759b2fa417fcdb514
Tuan N. Truong <tuan.truong625@gmail.com>
2021-02-04 22:40:03 -0500

    Get weather data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import moment from 'moment'
 
 import useToggle from '../src/utils/utils'
@@ -9,7 +10,10 @@ import TimeDetails from '../src/components/TimeDetails'
 function App() {
   const [showTimeDetails, setTimeDetails] = useToggle()
   const [date, setDate] = React.useState(new Date())
-  const [location, setLocation] = React.useState()
+  const [latitude, setLatitude] = React.useState('')
+  const [longitude, setLongitude] = React.useState('')
+  const [weather, setWeather] = React.useState('')
+  const apiKey = 'e4e0caaee4a4f16be0f9f4d58e05756f'
 
   const refreshClock = React.useCallback(() => {
     setInterval(() => {
@@ -17,34 +21,45 @@ function App() {
     }, 60 * 1000);
   }, [])
 
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude)
-      console.log(position.coords.longitude)
+  const getCurrentLocationData = React.useCallback(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
     })
-  }
+  }, [])
+
+  const getWeather = React.useCallback(async () => {
+    try {
+      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+      const data = response.data
+      setWeather(data)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }, [longitude, latitude])
 
   React.useEffect(() => {
     refreshClock();
-    getLocation();
+    getCurrentLocationData()
+    getWeather()
 
     return () => {
       clearInterval(refreshClock);
     }
-  }, [refreshClock])
+  }, [refreshClock, getCurrentLocationData, getWeather])
 
   const time = moment(date).format('HH:mm')
+
 
   return (
     <div className="container mx-auto">
       <div className="h-screen text-white grid grid-cols-3 gap-4">
         <Quote />
-
         <div className="col-span-full self-end flex flex-col sm:flex-row sm:justify-between	sm:items-end p-8 sm:p-24">
           <div className="location-info pb-5">
             <Greeting date={date} />
             <p className="py-5 font-bold text-8xl uppercase">{time}</p>
-            <p className="uppercase">in kitchener, ontario</p>
+            <p className="uppercase">in {weather.name}, ontario</p>
           </div>
 
           <div className="details-button pt-10">
